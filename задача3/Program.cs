53bf20caf8bb4c03c2a1c6d0e7c01eb2603b2743
Katya Panko <katena-1990@mail.ru>
2022-07-16 16:45:21 +0300

    задача3

---
+++ задача3/Program.cs
@@ -0,0 +1,52 @@
+﻿/* Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+
+int[,] InitMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random randomizer = new Random();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = randomizer.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите число m:");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число n:");
+int n = int.Parse(Console.ReadLine());
+
+int[,] matrix = InitMatrix(m, n);
+PrintMatrix(matrix);
+for (int i = 0; i < m; i++)
+{
+    double summ = 0;
+    for (int j = 0; j < n; j++)
+    {
+        summ = summ + matrix[j, i];
+    }
+    double avr = summ / m;
+    Console.Write(avr + " ; ");
+}
+
